09769d83a68433d9b29f431da5cf241faa3b31f7
nattawat14300 <nattawat14300@gmail.com>
2026-09-22 17:26:46 +0700

    finish 8

--- web programming/assignment8/shopping.js
+++ web programming/assignment8/shopping.js
@@ -1,10 +1,27 @@
 window.onload = pageLoad;
 
-function pageLoad(){
-	
+function pageLoad() {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "cloth.json");
+  xhr.onload = function () {
+    var jsdata = JSON.parse(xhr.responseText);
+    console.log(jsdata);
+    showData(jsdata);
+  };
+  xhr.onerror = function () {
+    alert("ERROR!");
+  };
+  xhr.send();
 }
 
-function showData(data){
-	
-}
+function showData(cloth) {
+    let box = document.getElementById("layer").children;
 
+    for (let i = 0; i < cloth.length; i++) {
+
+        box[i].innerHTML =
+            "<img src='" + cloth[i].img + "'>" +
+            "<p>" + cloth[i].brand + "</p>" +
+            "<p>Price: " + cloth[i].price + " Baht</p>";
+    }
+}
